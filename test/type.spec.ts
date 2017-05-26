5c7709ca9b100fb29c1a4879c1037fd66f9ea27c
Tim Baumann <tim@timbaumann.info>
2017-05-26 02:44:14 +0200

    Tests: Remove redundant ids

--- test/type.spec.ts
+++ test/type.spec.ts
@@ -18,74 +18,74 @@ describe('when adding type definition', () => {
   });
 
   it('should handle an array with one type param', () => {
-    const item = { type: "Array", id: "a", typeParams: [{ type: "string", id: "a.s" }] };
+    const item = { type: "Array", typeParams: [{ type: "string" }] };
     typeDef(env, item);
     env.sb.toString().should.equal("string[]")
   });
 
   it('should handle an union with one type param', () => {
-    const item = { type: "union", id: "u", typeParams: [{ type: "string", id: "u.s" }] };
+    const item = { type: "union", typeParams: [{ type: "string" }] };
     typeDef(env, item);
     env.sb.toString().should.equal("string")
   });
 
   it('should handle an union with two type params', () => {
-    const item = { type: "union", id: "u", typeParams: [{ type: "number", id: "u.n" }, { type: "string", id: "u.s" }] };
+    const item = { type: "union", typeParams: [{ type: "number" }, { type: "string" }] };
     typeDef(env, item);
     env.sb.toString().should.equal("number | string")
   });
 
   it('should handle an union with one array type param', () => {
-    const item = { type: "union", id: "u", typeParams: [{ type: "Array", id: "u.a", typeParams: [{type: "Node", id: "u.a.n"}] }] };
+    const item = { type: "union", typeParams: [{ type: "Array", typeParams: [{type: "Node"}] }] };
     typeDef(env, item);
     env.sb.toString().should.equal("Node[]")
   });
 
   it('should handle an union with one number param and one function', () => {
-    const item = { type: "union", id: "u", typeParams: [{ type: "number", id: "u.n" }, { type: "Function", id: "u.f", params: [{type: "string", id: "u.f.s"}] }] };
+    const item = { type: "union", typeParams: [{ type: "number" }, { type: "Function", params: [{type: "string"}] }] };
     typeDef(env, item);
     env.sb.toString().should.equal("number | ((p: string) => void)")
   });
 
   it('should handle an union with one number param and one function with two params', () => {
-    const item = { type: "union", id: "u", typeParams: [{ type: "number", id: "u.n" }, { type: "Function", id: "u.f", params: [{ type: "string", id: "u.f.s1" }, { type: "string", id: "u.f.s2" }] }] };
+    const item = { type: "union", typeParams: [{ type: "number" }, { type: "Function", params: [{ type: "string" }, { type: "string" }] }] };
     typeDef(env, item);
     env.sb.toString().should.equal("number | ((p1: string, p2: string) => void)")
   });
 
   it('should handle an object with unknown properties', () => {
-    const item = { type: "Object", id: "o" };
+    const item = { type: "Object", };
     typeDef(env, item);
     env.sb.toString().should.equal("Object")
   });
 
   it('should handle string', () => {
-    let item = { type: "string", id: "s" };
+    let item = { type: "string" };
     typeDef(env, item);
     env.sb.toString().should.equal("string")
   });
 
   it('should handle bool', () => {
-    let item = { type: "bool", id: "b" };
+    let item = { type: "bool" };
     typeDef(env, item);
     env.sb.toString().should.equal("boolean")
   });
 
   it('should handle other with one type param', () => {
-    const item = { type: "MyType", id: "m", typeParams: [{name: "typeParam1", type: "string", id: "m.s"}]};
+    const item = { type: "MyType", typeParams: [{name: "typeParam1", type: "string" }]};
     typeDef(env, item);
     env.sb.toString().should.equal("MyType<string>")
   });
 
   it('should handle other with two type params', () => {
-    const item = { type: "MyType", id: "m", typeParams: [{ name: "typeParam1", type: "string", id: "m.s" }, { name: "typeParam2", type: "number", id: "m.n" }]};
+    const item = { type: "MyType", typeParams: [{ name: "typeParam1", type: "string" }, { name: "typeParam2", type: "number" }]};
     typeDef(env, item);
     env.sb.toString().should.equal("MyType<string, number>")
   });
 
   describe('when type is unknown', () => {
     it('should replace type', () => {
-      const item = { type: "MyType", id: "m", typeParams: [{ name: "typeParam1", type: "string", id: "s.m" }, { name: "typeParam2", type: "number", id: "m.n" }] };
+      const item = { type: "MyType", typeParams: [{ name: "typeParam1", type: "string" }, { name: "typeParam2", type: "number" }] };
       typeDef(env, item);
       env.sb.toString().should.equal("MyType<string, number>")
     });
